098712b67cdcc5bc4f5c607c0e807b495b843952
zisadlier <zsadlier@umich.edu>
2018-02-12 15:35:54 -0500

    Added Altcoins

--- cryptodisplay/__main__.py
+++ cryptodisplay/__main__.py
@@ -14,42 +14,73 @@ from PyQt5.QtCore import *
 
 from coinmarketcap import Market
 
-def get_currency_attribute(currency, attribute, rounded_usd=False):
+def get_currency_attribute(currency, attribute, rounded_usd=False, btc=False):
     attributes = Market().ticker(currency)[0]
 
     if rounded_usd:
+        if btc:
+            return str(round(float(attributes[attribute]), 2)) + " (" + attributes['price_btc'] + ")"
+
         return str(round(float(attributes[attribute]), 2))
 
     return attributes[attribute]
 
-def get_change_label(currency, window):
+def get_change_label(currency, font, window):
+    price_usd = float(get_currency_attribute(currency, 'price_usd'))
+    change_percent = float(get_currency_attribute(currency, 'percent_change_24h'))
+    change_usd = price_usd * (change_percent/100)
+
+    change_str = "$" + str(round(change_usd, 2)) + " (" + str(change_percent) + "%)"
+    label = QLabel(change_str, window)
+    label.setFont(font)
+    if float(change_percent) >= 0:
+        label.setText("+ " + change_str)
+        label.setStyleSheet('color: #32CD32')
+    else:
+        label.setText("- " + change_str)
+        label.setStyleSheet('color: #F00')
+
+    return label
+
+def get_regular_label(text, font, window):
+    label = QLabel(text, window)
+    label.setFont(font)
+    label.setStyleSheet('color: #000')
+
+    return label
+
+def update_change_label(label, currency):
     price_usd = float(get_currency_attribute(currency, 'price_usd'))
     change_percent = float(get_currency_attribute(currency, 'percent_change_24h'))
     change_usd = price_usd * (change_percent/100)
 
     change_str = "$" + str(round(change_usd, 2)) + " (" + str(change_percent) + "%)"
 
-    label = QLabel(change_str, window)
+    label.setText(change_str)
     if float(change_percent) >= 0:
+        label.setText("+ " + change_str)
         label.setStyleSheet('color: #32CD32')
     else:
+        label.setText("- " + change_str)
         label.setStyleSheet('color: #F00')
 
-    return label
- 
 class CurrencyDisplay(QWidget):
  
-    def __init__(self, app, update_time=10, size_ratio=1):
+    def __init__(self, app, altcoin_size=150, update_time=10, size_ratio=1):
         super().__init__()
-        #self.layout = QGridLayout(self)
         self.title = 'Crypto display'
         self.update_time = update_time * 1000;
         self.size_ratio = size_ratio
+        self.altcoin_size = altcoin_size * size_ratio
         self.app = app
 
         self.logo_sizes = {}
         self.logo_sizes["btc"] = 192 * self.size_ratio
         self.logo_sizes["eth"] = 500 * self.size_ratio
+
+        self.altcoin_logos = []
+        self.altcoin_prices = []
+        self.altcoin_changes = []
 
         self.initImages()
         self.initText()
@@ -66,6 +97,7 @@ class CurrencyDisplay(QWidget):
         self.setGeometry(self.geometry)
         self.gh = self.geometry.height()
         self.gw = self.geometry.width()
+        self.spacing = self.gw/3
  
         # Background
         bg = QLabel(self)
@@ -81,59 +113,67 @@ class CurrencyDisplay(QWidget):
         eth_logo.setPixmap(QPixmap(os.path.join(IMAGE_DIR, 'ethereum.png')).scaled(self.logo_sizes["eth"], self.logo_sizes["eth"], Qt.KeepAspectRatio))
         eth_logo.move(self.gw/2.2, self.gh/2.5 - self.logo_sizes["eth"]/2)
 
+        # Altcoin logos
+        for i, altcoin in enumerate(ALTCOINS):
+            alt_label = QLabel(self)
+            alt_label.setPixmap(QPixmap(os.path.join(IMAGE_DIR, altcoin.lower()+'.png')).scaled(self.altcoin_size, self.altcoin_size, Qt.KeepAspectRatio))
+            alt_label.move(self.gw/100 + i*self.spacing, self.gh/1.15 - self.altcoin_size/2) 
+            self.altcoin_logos.append(alt_label)
 
 
     def initText(self):
-
+        # Font declarations
         date_font = QFont("Times", 48 * self.size_ratio, QFont.Bold) 
         big_value_font = QFont("Ariel", 64 * self.size_ratio, QFont.Bold)
+        small_value_font = QFont("Ariel", 24 * self.size_ratio, QFont.Bold)
         change_font = QFont("Ariel", 28 * self.size_ratio, QFont.Bold)  
+        small_change_font = QFont("Ariel", 16 * self.size_ratio, QFont.Bold)  
 
         # Date and time labels
-        self.date_lbl = QLabel(datetime.datetime.now().strftime("%B %d, %Y"), self)
-        self.date_lbl.setFont(date_font)
-        self.date_lbl.setStyleSheet('color: #000')
+        self.date_lbl = get_regular_label(datetime.datetime.now().strftime("%B %d, %Y"), date_font, self)
         text_width = self.date_lbl.fontMetrics().boundingRect(self.date_lbl.text()).width()
         self.date_lbl.move(self.gw/2 - text_width/2, self.gh/30)
 
-        self.time_lbl = QLabel(datetime.datetime.now().strftime("%H:%M"), self)
-        self.time_lbl.setFont(date_font)
-        self.time_lbl.setStyleSheet('color: #000')
+        self.time_lbl = get_regular_label(datetime.datetime.now().strftime("%H:%M"), date_font, self)
         text_width = self.time_lbl.fontMetrics().boundingRect(self.time_lbl.text()).width()
         self.time_lbl.move(self.gw/2 - text_width/2, self.gh/8)
 
         # BTC labels
-        self.btc_price_lbl = QLabel("$"+get_currency_attribute('Bitcoin', 'price_usd', rounded_usd=True), self)
-        self.btc_price_lbl.setFont(big_value_font)
-        self.btc_price_lbl.setStyleSheet('color: #000')
+        self.btc_price_lbl = get_regular_label("$"+get_currency_attribute('Bitcoin', 'price_usd', rounded_usd=True), big_value_font, self)
         self.btc_price_lbl.move(self.gw/6, self.geometry.height()/3)
 
-        self.btc_change_lbl = get_change_label('Bitcoin', self)
-        self.btc_change_lbl.setFont(change_font)
-        text_width = self.btc_change_lbl.fontMetrics().boundingRect(self.btc_change_lbl.text()).width()
-        self.btc_change_lbl.move(self.gw/6 + text_width/8, self.geometry.height()/2.2)
+        self.btc_change_lbl = get_change_label('Bitcoin', change_font, self)
+        self.btc_change_lbl.move(self.gw/6, self.geometry.height()/2.2)
 
         # ETH labels
-        self.eth_price_lbl = QLabel("$"+get_currency_attribute('Ethereum', 'price_usd', rounded_usd=True), self)
-        self.eth_price_lbl.setFont(big_value_font)
-        self.eth_price_lbl.setStyleSheet('color: #000')
+        self.eth_price_lbl = get_regular_label("$"+get_currency_attribute('Ethereum', 'price_usd', rounded_usd=True), big_value_font, self)
         self.eth_price_lbl.move(self.gw/1.47, self.geometry.height()/3)
 
-        self.eth_change_lbl = get_change_label('Ethereum', self)
-        self.eth_change_lbl.setFont(change_font)
-        text_width = self.eth_change_lbl.fontMetrics().boundingRect(self.eth_change_lbl.text()).width()
-        self.eth_change_lbl.move(self.gw/1.47 + text_width/8, self.geometry.height()/2.2)
+        self.eth_change_lbl = get_change_label('Ethereum', change_font, self)
+        self.eth_change_lbl.move(self.gw/1.47, self.geometry.height()/2.2)
+
+        # Altcoin labels
+        for i, altcoin in enumerate(ALTCOINS):
+            alt_price_lbl = get_regular_label("$"+get_currency_attribute(altcoin, 'price_usd', rounded_usd=True, btc=True), small_value_font, self)
+            alt_price_lbl.move(self.gw/10 + i*self.spacing, self.geometry.height()/1.25)
+            alt_change_lbl = get_change_label(altcoin, small_change_font, self)
+            alt_change_lbl.move(self.gw/10 + i*self.spacing, self.geometry.height()/1.15)
+
+            self.altcoin_prices.append(alt_price_lbl)
+            self.altcoin_changes.append(alt_change_lbl)
 
     def setText(self):
         self.date_lbl.setText(datetime.datetime.now().strftime("%B %d, %Y"))
         self.time_lbl.setText(datetime.datetime.now().strftime("%H:%M"))
         self.btc_price_lbl.setText("$"+get_currency_attribute('Bitcoin', 'price_usd'))
         self.eth_price_lbl.setText("$"+get_currency_attribute('Ethereum', 'price_usd'))
+        update_change_label(self.btc_change_lbl, 'Bitcoin')
+        update_change_label(self.eth_change_lbl, 'Ethereum')
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = CurrencyDisplay(app, size_ratio=SIZE_RATIO, update_time=UPDATE_TIME)
+    ex = CurrencyDisplay(app, altcoin_size=ALTCOIN_SIZE, size_ratio=SIZE_RATIO, update_time=UPDATE_TIME)
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
